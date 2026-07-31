c288fca8a43b3d420645cb2d284bab30af9cb7ef
sofakostenko07-maker <sofakostenko07@gmail.com>
2026-07-31 15:58:55 +0200

    Create load_to_sql.py
    added to sql/

---
+++ sql/load_to_sql.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sqlalchemy import create_engine, text
+
+USER = "..."
+PASSWORD = "..."
+HOST = "localhost"
+DB = "churn_project"
+
+engine = create_engine(f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DB}")
+
+def load_csv(table_name, csv_path):
+    print(f"Loading {csv_path} to table → {table_name} ...")
+
+    df = pd.read_csv(csv_path)
+
+    df.columns = [c.lower() for c in df.columns]
+
+    try:
+        df.to_sql(table_name, engine, if_exists="append", index=False)
+        print(f"Successfully dumped to {table_name}")
+    except Exception as e:
+        print(f"Error at {table_name}: {e}")
+
+def main():
+    print("Uploading to MySQL...\n")
+
+    load_csv("order_items", "data/order_items.csv")
+
+
+    print("\nALL DONE")
+
+if __name__ == "__main__":
+    main()
